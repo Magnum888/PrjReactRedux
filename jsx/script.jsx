778d3fb6c4c19e5b312c3cee2b3d9d723835b3d2
Magnum888 <K.O.M.888@i.ua>
2017-12-05 17:47:15 +0200

    state react js

--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,50 +1,41 @@
-//let heading = <h1> Hello World!!!!!!! </h1>;
-let value = 2;
-let background = {backgroundColor: "green"};
-
-class Title extends React.Component{
-    getUrl(){
-        return(
-            "https://google.com"
-        )
-    }
-    render(){
-        if (value === 1){
-            return(
-                <h1 title={this.props.title}>
-                    Hi my dear {this.props.userName} user {this.getUrl()}
-                </h1>
-            )
-        }else {
-            return(
-                <h2 style={background} className="title_name" title={this.props.title}>
-                    Hi my dear {this.props.userName} user {this.getUrl()}
-                </h2>
-            )
-        }
-
-    }
+const DigitalClock = function (props) {
+    return <div>{props.time}</div>
 }
-class HelloWorld extends React.Component{
+
+class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            currentTime: (new Date()).toLocaleString()
+        }
+        this.clockLauncher()
+    }
+
+    clockLauncher(){
+        setInterval(()=>{
+            this.setState({
+                currentTime: (new Date()).toLocaleString()
+            })
+        }, 1000)
+    }
+
     render(){
-        let my = "My";
         return(
             <div>
+                <div>
+                    <DigitalClock time={this.state.currentTime}/>
+                </div>
                 <h1>
-                    Hello {my} World!
+                    {this.state.currentTime}
                 </h1>
-                <h1>
-                    Time {new Date(Date.now()).toLocaleTimeString()} now
-                </h1>
+
             </div>
         )
     }
 }
 ReactDOM.render(
     <div>
-        <HelloWorld/>
-        <Title title="user" userName="Jim"/>
+        <Clock/>
     </div>,
-    //heading,
     document.getElementById("content")
 );
